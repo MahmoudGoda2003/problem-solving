83e36e64b9263dc5d6a1e051c1e6acac83af95f4
Mahmoud Goda <mahmoden17@gmail.com>
2022-05-24 21:47:00 +0300

    Add solution for problem `A - Elephant` Link: https://codeforces.com/contest/617/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/617/submission/158329919

---
+++ codeforces/617/A.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan =new Scanner(System.in);
+	    int x=scan.nextInt();
+	    if(x%5==0){
+	      System.out.println(x/5);  
+	    }else{
+	    
+	    
+	    System.out.println((x/5)+1);
+	    }
+	    
+	    
+	}
+}
