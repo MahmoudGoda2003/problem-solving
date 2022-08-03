329d2509631eb4fb47b47b3d2baa073442d89954
Mahmoud Goda <mahmoden17@gmail.com>
2022-08-03 03:02:00 +0300

    Add solution for problem `C - Little Girl and Maximum Sum` Link: https://codeforces.com/contest/276/problem/C Tags: data structures, greedy, implementation, sortings, *1500 Ref: https://codeforces.com/contest/276/submission/166750165

--- codeforces/276/C.cpp
+++ codeforces/276/C.cpp
@@ -1,108 +1,63 @@
+/******************************************************************************
+ 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+ 
+*******************************************************************************/
 #include <iostream>
-#include<bits/stdc++.h>
-using namespace std;
+#include<map>
+#include<algorithm>
+#include<cstring>
+#include<cmath>
 
-#define ll long long
-#define double long double
-#define MOD 1000000007
-#define PI 3.14159265358979323846264
-
-const ll N = 2e5;
-ll arr[N], seg[4*N], laz[4*N];
-
-void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
-
-void build (ll ind, ll sl, ll sr) {
-    if (sl == sr){
-        seg[ind] = arr[sl];
-        return;
+using namespace std;                                          
+// int n;vector <pair<int,long long>>arr;
+// int search (int x){
+//     int low =0;int high=n-1;int mid=0;int ans;
+//     while(low<=high){
+//         mid =low+(high-low)/2;
+//         if(arr[mid].first>x){
+//             high=mid-1;
+//         }else{
+//             ans=mid;
+//             low =mid+1;
+//         }
+//         
+//     }
+//     return ans;
+// }
+// 
+ 
+ 
+int main(){
+   int n,q;
+    cin>>n>>q;long long arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-    ll mid = sl + (sr - sl) / 2;
-    build(ind*2, sl, mid);
-    build(ind*2+1, mid+1, sr);
-    seg[ind] = seg[ind*2] + seg[ind*2+1];
-}
-
-void lazy (ll ind, ll sl, ll sr) {
-    if (laz[ind] != 0) {
-        seg[ind] += (sr - sl + 1) * laz[ind];
-        if (sl != sr) {
-            laz[ind * 2] += laz[ind];
-            laz[ind*2+1] += laz[ind];
-        }
-        laz[ind] = 0;
+    long long f[n+1];fill(f,f+n+1,0);
+    for(int i=0;i<q;i++){
+        int x,y;
+        cin>>x>>y;
+        
+        f[x-1]++;
+        f[y]--;
     }
-}
-
-void update(ll ind, ll sl, ll sr, ll ql, ll qr, ll val) {
-    if (qr < sl || ql > sr)
-        return;
-    lazy(ind, sl, sr);
-    if (ql <= sl && qr >= sr) {
-        seg[ind] += (sr - sl + 1) * val;
-        if (sl != sr) {
-            laz[ind*2] += val;
-            laz[ind*2+1] += val;
-        }
-        return;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=f[i];
+       f[i]=sum;
     }
-    ll mid = sl + (sr - sl) / 2;
-    update(ind*2, sl, mid, ql, qr, val);
-    update(ind*2+1, mid+1, sr, ql, qr, val);
-    seg[ind] = seg[ind*2] + seg[ind*2+1];
-}
-
-ll query(ll ind, ll sl, ll sr, ll ql, ll qr) {
-    if (qr < sl || ql > sr)
-        return 0;
-    lazy(ind, sl, sr);
-    if (ql <= sl && qr >= sr)
-        return seg[ind];
-    ll mid = sl + (sr - sl) / 2;
-    return query(ind*2, sl, mid, ql, qr) + query(ind*2+1, mid+1, sr, ql, qr);
-}
-
-void solve() {
-    ll n,q;
-    cin >> n>>q;
-
-    priority_queue<ll> pq;
-    for (int i = 0; i < n; ++i) {
-        int x;
-        cin>>x;
-        pq.push(x);
+    sort(arr,arr+n,greater<int>());
+    sort(f,f+n,greater<int>());
+    long long ans=0;
+    for(int i=0;i<n;i++){
+        ans+=arr[i]*f[i];
     }
-    vector<ll> vec(n);
-    while (q--) {
-        int l, r;
-        cin >> l >> r;
-        vec[l-1]++;
-        if (r-1<n-1)
-            vec[r]--;
-    }
-    for (int i = 1; i < n; ++i) {
-        vec[i]+=vec[i-1];
-    }
-    ll sum =0;
-    std::sort(vec.begin(), vec.end(),greater<ll>());
-    for (const auto &item: vec){
-        sum+=item*pq.top()*1ll;
-        pq.pop();
-    }
-    cout<<sum<<endl;
-}
-
-
-
-
-
-int main() {
-    fast();
-    int t=1;
-    //cin>>t;
-    while(t-->0){
-        solve();
-    }
+   cout<<ans;
+   
     return 0;
 }
-	        		     		 			    	 	 	
+
