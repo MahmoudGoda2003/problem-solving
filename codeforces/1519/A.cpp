510dd78e5399f76b6b82b6f2f5d8a0b43f2697ff
Mahmoud Goda <mahmoden17@gmail.com>
2022-08-14 11:38:00 +0300

    Add solution for problem `A - Red and Blue Beans` Link: https://codeforces.com/contest/1519/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1519/submission/168243703

---
+++ codeforces/1519/A.cpp
@@ -0,0 +1,56 @@
+/******************************************************************************
+ 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+ 
+*******************************************************************************/
+#include <iostream>
+#include<map>
+#include<algorithm>
+#include<cstring>
+#include<cmath>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+
+#define ll long long 
+using namespace std;                                          
+
+int search (int x,int n,vector<int> &arr){
+     int low =0;int high=n-1;int mid=0;int ans;
+     while(low<=high){
+         mid =low+(high-low)/2;
+         if(arr[mid]>x){
+             high=mid-1;
+         }else{
+             ans=mid;
+             low =mid+1;
+         }
+         
+     }
+     return ans;
+ }
+
+int main(){
+int n;cin>>n;
+for(int i=0;i<n;i++){
+    int x,y,z;
+    cin>>x>>y>>z;
+    
+ if((ll)min(x,y)*(z+1)>=max(x,y)){
+     cout<<"YES"<<endl;
+ }else{
+     cout<<"NO"<<endl;
+ }
+    
+    
+}
+
+
+
+    return 0;
+}
+
+
