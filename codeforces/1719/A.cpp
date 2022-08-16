e183483127c30929ed42e0a5b118315c7b7c868e
Mahmoud Goda <mahmoden17@gmail.com>
2022-08-16 18:07:00 +0300

    Add solution for problem `A - Chip Game` Link: https://codeforces.com/contest/1719/problem/A Tags: games, math, *800 Ref: https://codeforces.com/contest/1719/submission/168546273

---
+++ codeforces/1719/A.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+ 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+ 
+*******************************************************************************/
+#include <iostream>
+#include<map>
+#include<algorithm>
+#include<cstring>
+#include<cmath>
+#include<set>
+ 
+using namespace std;                                          
+ 
+int main()
+{
+ int t;cin>>t;
+ for(int k=0;k<t;k++){
+     int x,y;
+     cin>>x>>y;
+     if((long long)(x+y)%2==0){
+         cout<< "Tonya"<<endl;
+     }else{
+         cout<<"Burenka"<<endl;
+     }
+     
+     
+ }
+    return 0;
+}
+
