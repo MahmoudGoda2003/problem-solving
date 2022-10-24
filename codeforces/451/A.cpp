d63a5e208227ca5ed74ef25687a065a6bedaecd3
Mahmoud Goda <mahmoden17@gmail.com>
2022-10-25 00:23:00 +0300

    Add solution for problem `A - Game With Sticks` Link: https://codeforces.com/contest/451/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/451/submission/177810399

---
+++ codeforces/451/A.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+ 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+ 
+*******************************************************************************/
+#include <iostream>
+#include<map>
+#include<algorithm>
+#include<cstring>
+#include<cmath>
+ 
+using namespace std;                                          
+ 
+int main(){
+   int n,m;
+   cin>>n>>m;
+   n=min(n,m);
+   if (n%2==0){
+       cout<<"Malvika";
+   }else{
+       cout<<"Akshat";
+   }
+   
+   
+    return 0;
+}
+
