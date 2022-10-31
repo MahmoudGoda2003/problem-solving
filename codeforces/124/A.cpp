956a94f967ef57375ea4d1333ecc89442ae87a39
Mahmoud Goda <mahmoden17@gmail.com>
2022-10-31 20:30:00 +0200

    Add solution for problem `A - The number of positions` Link: https://codeforces.com/contest/124/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/124/submission/178728576

---
+++ codeforces/124/A.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+ 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+ 
+*******************************************************************************/
+#include <iostream>
+#include<map>
+#include<algorithm>
+#include<cstring>
+#include<cmath>
+ 
+using namespace std;                                          
+ 
+int main()
+{
+  
+       int n,a,b;
+       cin>>n>>a>>b;
+       cout<<n-max(a+1,n-b)+1;
+       
+    
+    return 0;
+}
+
