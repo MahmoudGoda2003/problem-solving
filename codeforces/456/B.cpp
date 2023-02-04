440bebb71dc10a1b5ea80c6828f48a944a9696b7
Mahmoud Goda <mahmoden17@gmail.com>
2023-02-04 04:11:00 +0200

    Add solution for problem `B - Fedya and Maths` Link: https://codeforces.com/contest/456/problem/B Tags: math, number theory, *1200 Ref: https://codeforces.com/contest/456/submission/192087250

---
+++ codeforces/456/B.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+set <long long> SieveOfEratosthenes()
+{
+    const int n = 1e6;
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+    set<long long> res;
+    for (int i = 2; i < 1e6+1; i++)
+    {
+        if (prime[i])
+            res.insert((long long)i * i);
+    }
+    return res;
+}
+
+
+int main() {
+
+    string x;
+    cin>>x;
+    int num = (int)x[x.length()-2]*10+(int)x[x.length()-1];
+    if(num%4==0){
+        cout<<4;
+    }else{
+        cout<<0;
+    }
+
+
+
+    return 0;
+}
