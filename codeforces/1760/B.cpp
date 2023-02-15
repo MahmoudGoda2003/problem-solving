4bc2ab9f16eb57e9e21ca2b93f1af277caaa51bb
Mahmoud Goda <mahmoden17@gmail.com>
2023-02-15 21:31:00 +0200

    Add solution for problem `B - Atilla's Favorite Problem` Link: https://codeforces.com/contest/1760/problem/B Tags: greedy, implementation, strings, *800 Ref: https://codeforces.com/contest/1760/submission/193760110

---
+++ codeforces/1760/B.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+set <long long> SieveOfEratosthenes()
+{
+    const int n = 1e6;
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+    set<long long> res;
+    for (int i = 2; i < 1e6+1; i++)
+    {
+        if (prime[i])
+            res.insert((long long)i * i);
+    }
+    return res;
+}
+
+
+int main() {
+    int t ;cin>>t;
+    while(t-->0){
+        int n;int mx=0;
+        cin>>n;string str;
+        cin>>str;
+        for (int i = 0; i < str.length(); ++i) {
+            mx= max(mx,int(str[i]-'a'));
+        }
+        cout<<mx+1<<endl;
+    }
+
+    return 0;
+}
