f25cee083b1e62ecd8ba9ccb6feb05c2c3390660
Mahmoud Goda <mahmoden17@gmail.com>
2023-05-14 03:19:00 +0300

    Add solution for problem `A - New Palindrome` Link: https://codeforces.com/contest/1832/problem/A Tags: strings, *800 Ref: https://codeforces.com/contest/1832/submission/205773210

---
+++ codeforces/1832/A.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+set <long long> SieveOfEratosthenes()
+{
+    const int n = 1e6;
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+    set<long long> res;
+    for (int i = 2; i < 1e6+1; i++)
+    {
+        if (prime[i])
+            res.insert((long long)i * i);
+    }
+    return res;
+}
+int mod(int a, int b) {
+    int result = a % b;
+    if (result < 0) {
+        result += b;
+    }
+    return result;
+}
+
+void solve() {
+    int n;
+    cin>>n;
+    string str;
+    for (int i = 0; i < n; ++i) {
+        bool flag = false;
+        cin>>str;
+        for (int j = 1; j < str.length()/2; ++j) {
+            if(str[j]!=str[0]){
+                flag = true;
+            }
+        }
+        if(flag){
+            cout<<"yes"<<endl;
+        }else{
+            cout<<"no"<<endl;
+        }
+    }
+}
+
+int main() {
+    int t=1;
+   // cin>>t;
+    while(t-->0){
+        solve();
+    }
+    return 0;
+}
