370eefd854211beb81de66bdb720de19b8beb362
Mahmoud Goda <mahmoden17@gmail.com>
2023-05-19 02:19:00 +0300

    Add solution for problem `B - Han Solo and Lazer Gun` Link: https://codeforces.com/contest/514/problem/B Tags: brute force, data structures, geometry, implementation, math, *1400 Ref: https://codeforces.com/contest/514/submission/206391429

---
+++ codeforces/514/B.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+set <long long> SieveOfEratosthenes()
+{
+    const int n = 1e6;
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+    set<long long> res;
+    for (int i = 2; i < 1e6+1; i++)
+    {
+        if (prime[i])
+            res.insert((long long)i * i);
+    }
+    return res;
+}
+int mod(int a, int b) {
+    int result = a % b;
+    if (result < 0) {
+        result += b;
+    }
+    return result;
+}
+
+void solve() {
+    int n;
+    cin>>n;
+    int x,y;
+    cin>>x>>y;
+    set<pair<int,int>> st;
+    for (int i = 0; i < n; ++i) {
+        int xx,yy;
+        cin>>xx>>yy;
+        xx-=x;yy-=y;
+        int g = __gcd(xx,yy);
+        st.insert({xx/g,yy/g});
+    }
+    cout<<st.size()<<endl;
+}
+
+int main() {
+    int t=1;
+    //cin>>t;
+    while(t-->0){
+        solve();
+    }
+    return 0;
+}
