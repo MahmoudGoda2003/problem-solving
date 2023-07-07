169260eabd2996a87ddf9b6ea7f57e00b3fc3d2c
Mahmoud Goda <mahmoden17@gmail.com>
2023-07-07 23:42:00 +0300

    Add solution for problem `A - Rudolph and Cut the Rope ` Link: https://codeforces.com/contest/1846/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1846/submission/212733916

--- codeforces/1846/A.cpp
+++ codeforces/1846/A.cpp
@@ -1,132 +1,249 @@
-#include <iostream>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <random>
+
 using namespace std;
+typedef long long ll;
+typedef long double ld;
 
-#define ll long long
-#define double long double
-#define MOD 1000000007
-#define M_PI 3.14159265358979323846264
+const ll N = 2e5 + 5, inf = LONG_MAX, mod = 1e9 + 7;
 
-set <long long> SieveOfEratosthenes()
-{
-    const int n = 1e6;
-    bool prime[n + 1];
-    memset(prime, true, sizeof(prime));
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
 
-    for (int p = 2; p * p <= n; p++) {
-        if (prime[p] == true) {
-            for (int i = p * p; i <= n; i += p)
-                prime[i] = false;
-        }
-    }
-    set<long long> res;
-    for (int i = 2; i < 1e6+1; i++)
-    {
-        if (prime[i])
-            res.insert((long long)i * i);
-    }
+ll mul( ll a,  ll b){
+    return (1LL * a * b)%mod;
+}
+
+ll add(ll a , ll b){
+    return a + b %mod;
+}
+
+ll fp( ll b , ll p){
+    if (p < 0)
+        return 0;
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp);
+    if(p&1)
+        temp = mul(temp,b);
+    return temp%mod;
+}
+
+ll fact(ll n){
+    ll res = 1;
+    for (int i = 2; i <= n; ++i)
+        res *= i;
     return res;
 }
-double calculateDistance(long long x1, long long y1, long long x2, long long y2) {
-    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+
+ll inv(ll i){
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
 }
-int mod(int a, int b) {
-    int result = a % b;
-    if (result < 0) {
-        result += b;
+
+ll mex(vector<ll> a){
+    set<ll> s;
+    ll n = (ll)a.size();
+
+    for (int i = 0; i < n; ++i)
+        s.insert(a[i]);
+
+    ll cu = 0;
+    for(auto ch : s){
+        if (ch != cu)
+            break;
+        cu++;
     }
-    return result;
-}
-double calculateTriangleArea(double angleA, double angleB, double sideLength) {
-    // Convert the angles from degrees to radians
-    double angleARad = (angleA * M_PI) / 180.0;
-    double angleBRad = (angleB * M_PI) / 180.0;
 
-    // Calculate the angle C
-    double angleC = 180.0 - angleA - angleB;
-
-    // Convert the angle C from degrees to radians
-    double angleCRad = (angleC * M_PI) / 180.0;
-
-    double l = (sideLength * sin(angleARad))/sin(angleCRad);
-
-    // Calculate the area of the triangle
-    double area = 0.5 * l * sideLength * sin(angleBRad);
-
-    return area;
+    return cu;
 }
 
-enum class State { Unvisited, Visiting, Visited };
+bool compareBySize(const vector<ll>& vec1, const vector<ll>& vec2) {
+    return vec1.size() > vec2.size();
+}
 
-bool hasCycleDFS(int node, const vector<vector<int>>& graph, vector<State>& state) {
-    state[node] = State::Visiting;
-
-    for (int neighbor : graph[node]) {
-        if (state[neighbor] == State::Unvisited) {
-            if (hasCycleDFS(neighbor, graph, state)) {
-                return true;
+vector<vector<ll>> sieveOfEratosthenes(ll n) {
+    vector<bool> isPrime(n + 1, true);
+    vector<vector<ll>> factors(n+1);
+    for (int i = 2; i * i <= n; i++) {
+        factors[i].push_back(i);
+        if (isPrime[i]) {
+            for (int j = 2 * i; j <= n; j += i) {
+                isPrime[j] = false;
+                factors[j].push_back(i);
+                factors[i].push_back(j);
             }
-        } else if (state[neighbor] == State::Visiting) {
-            return true;
+        }
+    }
+    return factors;
+}
+
+vector<ll> dis(3e5+10, inf), par(3e5+10);
+void bfs(ll node, vector<vector<ll>> &v, map<ll,bool> &m){
+    for (int i = 0; i < 3e5+10; ++i) {
+        par[i] = i;
+    }
+    queue<ll> q;
+    q.push(node);
+    dis[node] = 1;
+    map<ll, bool> h;
+
+    while (!q.empty()) {
+        node = q.front();
+        q.pop();
+
+        for(auto child : v[node]){
+            if (m[node] && m[child] && dis[child] > dis[node] + 1) {
+                par[child] = node;
+                dis[child] = dis[node] + 1;
+                q.push(child);
+            } else if (m[node] && (!m[child] && child < node) && dis[child] > dis[node]) {
+                par[child] = node;
+                dis[child] = dis[node];
+                q.push(child);
+            } else if (!m[node] && m[child] && dis[child] > dis[par[node]] + 1){
+                par[child] = par[node];
+                dis[child] = dis[par[node]] + 1;
+                q.push(child);
+            }
+        }
+    }
+}
+
+//bool vis[N];
+//ll dfs(ll node, vector<vector<ll>> &adj, vector<ll> &val){
+//    vis[node] = true;
+//    for (auto child : adj[node]){
+//        if (!vis[child])
+//            val[node] += dfs(child, adj, val);
+//    }
+//    if (!val[node])
+//        val[node]++;
+//    return val[node];
+//}
+
+vector<ll> divisors(ll n) {
+    vector<ll> divisors;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            divisors.push_back(i);
+            if (i != n/i)
+                divisors.push_back(n/i);
         }
     }
 
-    state[node] = State::Visited;
-    return false;
+    return divisors;
 }
 
-bool hasCycle(const vector<vector<int>>& graph) {
-    int n = graph.size();
-    vector<State> state(n, State::Unvisited);
+map<ll, ll> prime_factorize(ll n) {
+    map<ll, ll> factors;
+    ll i = 2;
 
-    for (int i = 0; i < n; ++i) {
-        if (state[i] == State::Unvisited && hasCycleDFS(i, graph, state)) {
-            return true;
+    while (i * i <= n) {
+        if (n % i == 0) {
+            n /= i;
+            factors[i]++;
+        } else {
+            i++;
         }
     }
 
-    return false;
-}
-long long power(long long base, long long exponent) {
-    long long result = 1;
-    base %= MOD;
-
-    while (exponent > 0) {
-        if (exponent % 2 == 1) {
-            result = (result * base) % MOD;
-        }
-
-        base = (base * base) % MOD;
-        exponent /= 2;
+    if (n > 1) {
+        factors[n]++;
     }
 
-    return result;
+    return factors;
 }
 
+void insertSorted(deque<ll>& sortedArray, ll element) {
+    auto it = lower_bound(sortedArray.begin(), sortedArray.end(), element);
+    sortedArray.insert(it, element);
+}
 
-
-void solve() {
-  int n ;
-  cin>>n;
-  int count =0;
-    for (int i = 0; i < n; ++i) {
-        int x,y;
-        cin>>x>>y;
-        if(x>y){
-            count++;
+int lis(vector<int>& nums) {
+    vector<int> dp;
+    for (int num : nums) {
+        auto it = lower_bound(dp.begin(), dp.end(), num);
+        if (it == dp.end()) {
+            dp.push_back(num);
+        } else {
+            *it = num;
         }
     }
-    cout<<count<<endl;
+    return dp.size();
 }
 
+ll phi(ll n) {
+    float result = n;
+    for(ll p = 2; p * p <= n; ++p) {
+        if (n % p == 0) {
+            while (n % p == 0)
+                n /= p;
+            result *= (1.0 - (1.0 / (float)p));
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return (ll)result;
+}
+//
+//ll dfs(ll node, vector<vector<ll>>& a, vector<bool>& vis, map<ll, bool> mapp){
+//    vis[node] = true;
+//    ll cnt = 0;
+//    for (ll child : a[node]){
+//        if (!vis[child])
+//            cnt += dfs(child, a, vis, mapp);
+//    }
+//    return max(cnt, mapp[node]?1ll:0ll);
+//}
+//
+//bool cmp(const pair<ll, ll>& a, const pair<ll, ll>& b) {
+//    if (a.first < b.first) {
+//        return true;
+//    }
+//    else if (a.first > b.first) {
+//        return false;
+//    }
+//    return a.second > b.second;
+//}
 
-
+//void generate(const vector<ll>& elements, ll n, vector<vector<ll>>& permutations, vector<ll>& current, vector<bool>& used) {
+//    if (current.size() == n) {
+//        permutations.push_back(current);
+//        return;
+//    }
+//
+//    for (int i = 0; i < elements.size(); ++i) {
+//        if (!used[i]) {
+//            used[i] = true;
+//            current.push_back(elements[i]);
+//            generate(elements, n, permutations, current, used);
+//            current.pop_back();
+//            used[i] = false;
+//        }
+//    }
+//}
 
 int main() {
-    int t=1;
-    cin>>t;
-    while(t-->0){
-        solve();
+    fast();
+    ll t;
+    cin >> t;
+//    t = 1;
+
+    while (t--) {
+        ll n, x, y;
+        string s, r;
+        bool done = false;
+        cin >> n;
+
+        ll cnt = 0;
+        for (int i = 0; i < n; ++i) {
+            cin >> x >> y;
+            if (x > y)
+                cnt++;
+        }
+
+        cout << cnt << endl;
     }
+
     return 0;
 }
