1d9c28a074a9246c6dbec88281d06c1aa982357d
Mahmoud Goda <mahmoden17@gmail.com>
2023-07-14 22:01:00 +0300

    Add solution for problem `A - Repeating Cipher` Link: https://codeforces.com/contest/1095/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1095/submission/213822191

---
+++ codeforces/1095/A.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define double long double
+#define MOD 1000000007
+#define PI 3.14159265358979323846264
+
+const ll N = 2e5;
+ll arr[N], seg[4*N], laz[4*N];
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+void build (ll ind, ll sl, ll sr) {
+    if (sl == sr){
+        seg[ind] = arr[sl];
+        return;
+    }
+    ll mid = sl + (sr - sl) / 2;
+    build(ind*2, sl, mid);
+    build(ind*2+1, mid+1, sr);
+    seg[ind] = max(seg[ind*2] , seg[ind*2+1]);
+}
+
+void lazy (ll ind, ll sl, ll sr) {
+    if (laz[ind] != 0) {
+        seg[ind] += (sr - sl + 1) * laz[ind];
+        if (sl != sr) {
+            laz[ind * 2] += laz[ind];
+            laz[ind*2+1] += laz[ind];
+        }
+        laz[ind] = 0;
+    }
+}
+
+void update(ll ind, ll sl, ll sr, ll ql, ll qr, ll val) {
+    if (qr < sl || ql > sr)
+        return;
+    lazy(ind, sl, sr);
+    if (ql <= sl && qr >= sr) {
+        seg[ind] += (sr - sl + 1) * val;
+        if (sl != sr) {
+            laz[ind*2] += val;
+            laz[ind*2+1] += val;
+        }
+        return;
+    }
+    ll mid = sl + (sr - sl) / 2;
+    update(ind*2, sl, mid, ql, qr, val);
+    update(ind*2+1, mid+1, sr, ql, qr, val);
+    seg[ind] = max(seg[ind*2] , seg[ind*2+1]);
+}
+
+
+int query(int ind, int sl, int sr, int ql, int qr, int k) {
+    if (qr < sl || ql > sr)
+        return 0;
+    lazy(ind, sl, sr);
+    if (ql <= sl && qr >= sr) {
+        if (seg[ind] <= k)
+            return 0;
+        if (sl == sr)
+            return 1;
+    }
+    int mid = sl + (sr - sl) / 2;
+    int leftCount = query(ind * 2, sl, mid, ql, qr, k);
+    int rightCount = query(ind * 2 + 1, mid + 1, sr, ql, qr, k);
+    return leftCount + rightCount;
+}
+
+void solve() {
+    int n;
+    cin>>n;
+    string str;
+    cin>>str;
+    for (int i = 0; i*(i+1)/2 < n; ++i){
+        cout << str[i*(i+1)/2];
+    }
+}
+
+
+
+
+
+int main() {
+    fast();
+    int t=1;
+    //cin>>t;
+    while(t-->0){
+        solve();
+    }
+    return 0;
+}
