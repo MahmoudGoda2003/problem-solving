4ea72c9881fb018cd152cfa452a9305f252cc717
Mahmoud Goda <mahmoden17@gmail.com>
2023-07-21 22:43:00 +0300

    Add solution for problem `A - To My Critics` Link: https://codeforces.com/contest/1850/problem/A Tags: implementation, sortings, *800 Ref: https://codeforces.com/contest/1850/submission/214955852

---
+++ codeforces/1850/A.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll unsigned long long
+#define double long double
+#define MOD 1000000007
+#define PI 3.14159265358979323846264
+
+const ll N = 1;
+ll arr[N], seg[4*N], laz[4*N];
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+void build (ll ind, ll sl, ll sr) {
+    if (sl == sr){
+        seg[ind] = arr[sl];
+        return;
+    }
+    ll mid = sl + (sr - sl) / 2;
+    build(ind*2, sl, mid);
+    build(ind*2+1, mid+1, sr);
+    seg[ind] = seg[ind*2] + seg[ind*2+1];
+}
+
+void lazy (ll ind, ll sl, ll sr) {
+    if (laz[ind] != 0) {
+        seg[ind] += (sr - sl + 1) * laz[ind];
+        if (sl != sr) {
+            laz[ind * 2] += laz[ind];
+            laz[ind*2+1] += laz[ind];
+        }
+        laz[ind] = 0;
+    }
+}
+
+void update(ll ind, ll sl, ll sr, ll ql, ll qr, ll val) {
+    if (qr < sl || ql > sr)
+        return;
+    lazy(ind, sl, sr);
+    if (ql <= sl && qr >= sr) {
+        seg[ind] += (sr - sl + 1) * val;
+        if (sl != sr) {
+            laz[ind*2] += val;
+            laz[ind*2+1] += val;
+        }
+        return;
+    }
+    ll mid = sl + (sr - sl) / 2;
+    update(ind*2, sl, mid, ql, qr, val);
+    update(ind*2+1, mid+1, sr, ql, qr, val);
+    seg[ind] = seg[ind*2] + seg[ind*2+1];
+}
+
+ll query(ll ind, ll sl, ll sr, ll ql, ll qr) {
+    if (qr < sl || ql > sr)
+        return 0;
+    lazy(ind, sl, sr);
+    if (ql <= sl && qr >= sr)
+        return seg[ind];
+    ll mid = sl + (sr - sl) / 2;
+    return query(ind*2, sl, mid, ql, qr) + query(ind*2+1, mid+1, sr, ql, qr);
+}
+
+ll fact(int n){
+    ll res=1;
+    while(n>0){
+        res*=n;
+        n--;
+    }
+    return res;
+}
+
+void solve() {
+  int a ,b,c;
+  cin>>a>>b>>c;
+  if(a+b>=10||a+c>=10||b+c>=10){
+    cout<<"YES"<<endl;
+  }else{
+      cout<<"NO"<<endl;
+  }
+
+
+}
+
+
+
+
+
+int main() {
+    fast();
+    int t=1;
+    cin>>t;
+    while(t-->0){
+        solve();
+    }
+    return 0;
+}
